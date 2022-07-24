615d401a9615a3d05ee0c3f930783b73f4bee15c
KarthikShetty11 <kvshetty27@gmail.com>
2022-07-24 15:46:17 +0530

    components updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TranscationList from './components/TranscationList';
-import AddTranscation from './components/AddTransaction';
+import AddTransaction from './components/AddTransaction';
+import GlobalProvider from './context/GlobalState';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
+    
       <Header />
+      
       <div className="container">
       <Balance />
       <IncomeExpenses />
-      </div>
+      <AddTransaction />
+      </div>  
+     
       <TranscationList />
-    </div>
+    </GlobalProvider>
+    
   );
 }
 
